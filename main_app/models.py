02f693848a8fcf5f10b21952124b8209486464d1
CatW-M <wheelermushlin@gmail.com>
2022-12-07 22:35:26 -0700

    feat: Item Model

--- main_app/models.py
+++ main_app/models.py
@@ -1,8 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Item(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     significance = models.CharField(max_length=250)
-    date_posted = models.DateTimeField('date listed')
-    active = models.BooleanField(True)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Item
+
+class ItemCreate(CreateView):
+    model: Item
+
 
 def index(request):
     return render(request, 'index.html')
+
+def items_index(request):
+    items=Item.objects.all()
+    return render(request, 'items/index.html', {'items': items})
