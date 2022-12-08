bb9e3b64f984ef0c725191adb6554c5d0a8b53af
CatW-M <wheelermushlin@gmail.com>
2022-12-07 21:50:52 -0700

    feat: added home page

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    significance = models.CharField(max_length=250)
+    date_posted = models.DateTimeField('date listed')
+    active = models.BooleanField(True)
