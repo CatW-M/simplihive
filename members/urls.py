d8bb1f672929260b8e5afb2ec6145d569a8b0a7c
CatW-M <wheelermushlin@gmail.com>
2022-12-17 18:23:29 -0700

    Added Profile Model

--- members/urls.py
+++ members/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('login_user', views.login_user, name="login"),
     path('logout_user', views.logout_user, name="logout"),
     path('signup', views.signup, name='signup'),
-]
+    path('profile/', views.profile, name='profile'),
+    ]
--- members/views.py
+++ members/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from main_app.models import Item, User
 
 # Create your views here.
 def login_user(request):
@@ -12,7 +13,7 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('index')
+            return redirect('profile', {'username':username})
         else:
             messages.success(request, ("There was an Error logging in, Try Again..."))
             return redirect('login')
@@ -38,3 +39,7 @@ def signup(request):
     else:
         form = SignUpForm()
     return render(request, 'authenticate/signup.html', {'form': form,})
+
+@login_required
+def profile(request):
+    return render(request, 'authenticate/profile.html')
