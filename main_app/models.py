b7b8a659c03f26860cf43ba0529dff8c79cbb512
CatW-M <wheelermushlin@gmail.com>
2022-12-31 20:11:00 -0700

    modified item model

--- main_app/models.py
+++ main_app/models.py
@@ -22,7 +22,7 @@ class Item(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     item_image = models.ImageField(null=True, blank=True, upload_to='images/')
     anonymous = models.BooleanField(default=False)
-    status = models.ForeignKey(Status, on_delete=models.CASCADE)
+    status = models.CharField(max_length=50, blank=True, null=True)
 
     def __str__(self):
         return self.name
